746196bc16fadee28e116a20ff0b581d23558124
HashirRehman <meethashirrehman@gmail.com>
2024-09-25 16:38:54 +0500

    Handle web resources

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,20 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-react"]
+  modules: ["@wxt-dev/module-react"],
+  manifest: {
+    web_accessible_resources: [
+      {
+        matches: ["*://*.linkedin.com/*"],
+        resources: [
+          "Frame.svg",
+          "regenerate.svg",
+          "generate.svg",
+          "insert.svg",
+        ],
+      },
+    ],
+    permissions: ["storage", "tabs"],
+    host_permissions: ["*://*.google.com/*"],
+  },
 });
