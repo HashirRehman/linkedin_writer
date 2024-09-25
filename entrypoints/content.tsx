f8a16c94311e4897ab6eeb5c6d2f1a8326b2e74a
HashirRehman <meethashirrehman@gmail.com>
2024-09-25 14:29:46 +0500

    Add button icons and minor UI improvements

--- entrypoints/content.tsx
+++ entrypoints/content.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom"
 import "./style.css"
-import MySVGIcon from '../public/Frame.svg'
-import generate from '../public/generate.svg'
+import MySVGIcon from "../public/Frame.svg"
+import Generate from "../public/generate.svg"
+import Insert from "../public/insert.svg"
+import Regenerate from "../public/regenerate.svg"
 
 const LinkedInExtension: React.FC = () => {
   const [modalVisible, setModalVisible] = useState(false)
@@ -42,7 +44,8 @@ const LinkedInExtension: React.FC = () => {
         <img src=${MySVGIcon} alt="Icon" width="32" height="32" />
       `
       svgContainer.addEventListener("click", () => {
-        setStage('prompt')
+        setStage("prompt")
+        setMessages([])
         setModalVisible(true)
       })
       targetElement.parentElement?.appendChild(svgContainer)
@@ -51,11 +54,14 @@ const LinkedInExtension: React.FC = () => {
 
   const handleGenerate = () => {
     if (userInput.trim()) {
-      setMessages((prev) => [...prev, `${userInput}`])
+      setMessages((prev) => [...prev, `You ${userInput}`])
       setUserInput("")
 
       setTimeout(() => {
-        setMessages((prev) => [...prev, "Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask"])
+        setMessages((prev) => [
+          ...prev,
+          "Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask",
+        ])
       }, 1000)
       setStage("chat")
     }
@@ -67,7 +73,7 @@ const LinkedInExtension: React.FC = () => {
     ) as HTMLDivElement
 
     if (messageInput) {
-      console.log("Messages,",messages)
+      console.log("Messages,", messages)
       messageInput.innerHTML = `<p>${messages[1]}</p>`
       const inputEvent = new Event("input", { bubbles: true })
       messageInput.dispatchEvent(inputEvent)
@@ -80,7 +86,9 @@ const LinkedInExtension: React.FC = () => {
     setModalVisible(false)
   }
 
-  const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const handleBackdropClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
     const modalContent = document.getElementById("modal-content")
     if (modalContent && !modalContent.contains(event.target as Node)) {
       hideModal()
@@ -101,67 +109,95 @@ const LinkedInExtension: React.FC = () => {
         >
           <div
             id="modal-content"
-            className={`bg-white p-6 rounded-lg shadow-lg ${stage === "prompt" ? 'h-[110px]' : 'h-auto'} w-[500px] relative shadow-[0px_10px_15px_-3px_#0000001A]`}
+            className={`bg-white p-6 rounded-lg shadow-lg ${
+              stage === "prompt" ? "h-[110px]" : "h-auto"
+            } w-[500px] relative shadow-[0px_10px_15px_-3px_#0000001A]`}
           >
             {stage === "prompt" ? (
-             <div>
-             <input
-               type="text"
-               placeholder="Your Prompt"
-               value={userInput}
-               onChange={(e) => setUserInput(e.target.value)}
-               className="border w-full p-2 mb-4"
-             />
-             <div className="flex justify-end">
-               <button
-                 onClick={handleGenerate}
-                 className="bg-blue-500 text-white px-4 py-2 flex items-center gap-2 rounded-lg hover:bg-blue-600"
-               >
-                 <img className="inline-block" src={generate} alt="Icon" width="20" height="20" />
-                 <span>Generate</span>
-               </button>
-             </div>
-           </div>
-
-            ) : (
               <div>
+                <input
+                  type="text"
+                  placeholder="Your Prompt"
+                  value={userInput}
+                  onChange={(e) => setUserInput(e.target.value)}
+                  className="border w-full p-2 mb-4"
+                />
+                <div className="flex justify-end">
+                  <button
+                    onClick={handleGenerate}
+                    className="bg-blue-500 text-white px-4 py-2 flex items-center gap-2 rounded-lg hover:bg-blue-600"
+                  >
+                    <img
+                      className="inline-block"
+                      src={Generate}
+                      alt="Icon"
+                      width="20"
+                      height="20"
+                    />
+                    <span>Generate</span>
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <>
                 <div
                   id="chat-window"
                   className="border h-64 mb-4 p-4 overflow-y-auto flex flex-col space-y-2"
                 >
-                  {messages.map((msg, index) => (
-                    <div
-                      key={index}
-                      className={
-                        index === 0
-                          ? "bg-gray-100 p-2 rounded-lg self-end text-gray-custom"
-                          : "!bg-blue-100 p-2 rounded-lg self-start text-gray-custom"
-                      }
-                    >
-                      {msg}
-                    </div>
-                  ))}
+                  {messages.map((msg, index) => {
+                    const isFromUser = msg.startsWith("You")
+                    const strippedMsg = isFromUser
+                      ? msg.replace(/^You\s*/, "")
+                      : msg
+                    const messageClass = isFromUser
+                      ? "bg-gray-100 p-2 rounded-lg self-end text-gray-custom w-auto"
+                      : "!bg-blue-100 p-2 rounded-lg self-start text-gray-custom w-5/6"
+
+                    return (
+                      <div key={index} className={messageClass}>
+                        {strippedMsg}
+                      </div>
+                    )
+                  })}
                 </div>
-                <input
-               type="text"
-               placeholder="Your Prompt"
-               value={userInput}
-               onChange={(e) => setUserInput(e.target.value)}
-               className="border w-full p-2 mb-4"
-             />
-                <button
-                  onClick={handleGenerate}
-                  className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-                >
-                  Regenerate
-                </button>
-                <button
-                  onClick={insertIntoLinkedIn}
-                  className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 mt-4"
-                >
-                  Insert into LinkedIn
-                </button>
-              </div>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Your Prompt"
+                    value={userInput}
+                    onChange={(e) => setUserInput(e.target.value)}
+                    className="border w-full p-2 mb-4"
+                  />
+                </div>
+                <div className="flex justify-end gap-4">
+                  <button
+                    onClick={handleGenerate}
+                    className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+                  >
+                    <img
+                      className="inline-block"
+                      src={Regenerate}
+                      alt="Icon"
+                      width="10"
+                      height="10"
+                    />
+                    <span className="px-2">Regenerate</span>
+                  </button>
+                  <button
+                    onClick={insertIntoLinkedIn}
+                    className="bg-transparent border-[1px] border-solid border-[var(--Colors-Gray-500)] text-[rgba(102,109,128,1)] px-4 py-2 rounded-lg"
+                  >
+                    <img
+                      className="inline-block"
+                      src={Insert}
+                      alt="Icon"
+                      width="10"
+                      height="10"
+                    />
+                    <span className="px-2">Insert</span>
+                  </button>
+                </div>
+              </>
             )}
           </div>
         </div>
